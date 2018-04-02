5450a16ded5efee9e5efef47bcb90a52393e021e
Mikael DAUTREY <mikael.dautrey@isitix.com>
2018-04-02 15:16:53 +0200

    gestion de la date

--- docs/assets/js/jquery.js
+++ docs/assets/js/jquery.js
@@ -20,19 +20,4 @@ $(document).ready(() => {
         $('#teamPresentation').toggle();
 
     })
-    function getCompareDate() {
-        var d = new Date(),
-            month = '' + (d.getMonth() + 1),
-            day = '' + d.getDate(),
-            year = d.getFullYear();
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-        return [year, month, day].join('');
-    }
-    $('[future-date]').each(function() {
-        if($(this).attr('future-date') > getCompareDate()){ 
-            $(this).hide();
-            alert(getCompareDate());
-        }
-    });
 });
